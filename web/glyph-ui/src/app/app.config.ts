0ac7bc946c3867e9a65f0a0cdbe4090db3e9a6bf
Light Play <lightplay456@gmail.com>
2026-06-12 13:22:11 +0300

    feat(web): добавлены 3 Interceptors (добавление куки к каждому запросу, формирования url для запроса, редирект на SSO при unauthorized статусе http ответа)

--- web/glyph-ui/src/app/app.config.ts
+++ web/glyph-ui/src/app/app.config.ts
@@ -1,10 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { bffBaseUrlInterceptor } from '../core/interceptors/bff-base-url.interceptor';
+import { credentialsInterceptor } from '../core/interceptors/credentials.interceptor';
+import { authErrorInterceptor } from '../core/interceptors/auth-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([bffBaseUrlInterceptor, credentialsInterceptor, authErrorInterceptor])),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
