2eb24defb78dc07422bfc82e2b7a365b86864972
Light Play <lightplay456@gmail.com>
2026-06-12 14:52:02 +0300

    feat: реализован функционал бесшовного обновление Access и Refresh токенов. Автоматическое продление жизни cookie. Общие ключи шифрования через библиотеку Microsoft.AspNetCore.DataProtection.StackExchangeRedis для SSO авторизации.

--- backend/src/apps/Glyph.Bff/Program.cs
+++ backend/src/apps/Glyph.Bff/Program.cs
@@ -1,6 +1,133 @@
+using System.Reflection;
+using System.Text.Json;
+using Glyph.Bff.Extensions;
+using Glyph.Bff.Infrastructure.Clients;
+using Glyph.Bff.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+using Shared.Contracts.Authentication.Requests;
+using Shared.Redis;
+using Shared.Redis.Common;
+using Shared.Web.Extensions;
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+builder.Services.Configure<JsonSerializerOptions>(opt => opt.AddCrossdyneDefaults());
+
+builder.Services.AddAuthorization();
+builder.Services.AddServices(builder.Configuration).AddHttpClients(builder.Configuration);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalFrontend", policy =>
+    {
+        policy.WithOrigins("http://127.0.0.1:4201", "https://glyph.crossdyne.com")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
+var redisConnectionString = builder.Configuration["Redis:ConnectionString"];
+var redis = ConnectionMultiplexer.Connect(redisConnectionString!);
+
+builder.Services.AddDataProtection()
+    .SetApplicationName("Crossdyne.SharedBff")
+    .PersistKeysToStackExchangeRedis(redis, "Crossdyne-DataProtection-Keys");
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "Crossdyne";
+        options.ExpireTimeSpan = TimeSpan.FromDays(30);
+        options.Cookie.HttpOnly = true;
+        options.SlidingExpiration = true;
+
+        if (builder.Environment.IsDevelopment())
+        {
+            options.Cookie.Domain = "127.0.0.1"; 
+            options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest; 
+            options.Cookie.SameSite = SameSiteMode.Lax;
+        }
+        else
+        {
+            options.Cookie.Domain = ".crossdyne.com"; 
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            options.Cookie.SameSite = SameSiteMode.Strict;
+        }
+
+        options.Events.OnRedirectToLogin = context =>
+        {
+            context.Response.StatusCode = 401;
+            return Task.CompletedTask;
+        };
+
+        options.Events.OnRedirectToAccessDenied = context =>
+        {
+            context.Response.StatusCode = 403;
+            return Task.CompletedTask;
+        };
+
+        options.Events.OnValidatePrincipal = async context =>
+        {
+            var sessionId = context.Principal?.FindFirst("SessionId")?.Value;
+
+            if (string.IsNullOrWhiteSpace(sessionId))
+            {
+                context.RejectPrincipal();
+                return;
+            }
+
+            var cache = context.HttpContext.RequestServices.GetRequiredService<IRedisCacheService>();
+            var session = await cache.GetJsonAsync<UserSession>(sessionId);
+           
+            if (session == null)
+            {
+                context.RejectPrincipal();
+                return;
+            }
+
+            if (session.AccessTokenExpiresAt <= DateTime.UtcNow.AddMinutes(1))
+            {
+                var authClient = context.HttpContext.RequestServices.GetRequiredService<IAuthClient>();
+
+                var refreshResult = await authClient.RefreshTokens(new RefreshTokensRequest(session.RefreshToken, session.AccessToken));
+
+                if (refreshResult.IsSuccess)
+                {
+                    var jwtReader = context.HttpContext.RequestServices.GetRequiredService<IJwtReadService>();
+                    var jwtData = jwtReader.ExtractData(refreshResult.Value.AccessToken);
+
+                    session.AccessToken = refreshResult.Value.AccessToken;
+                    session.RefreshToken = refreshResult.Value.RefreshToken;
+                    session.AccessTokenExpiresAt = jwtData.ExpiredTime;
+
+                    await cache.SetJsonAsync(sessionId, session, TimeSpan.FromDays(30));
+                }
+                else
+                {
+                    var updatedSession = await cache.GetJsonAsync<UserSession>(sessionId);
+
+                    if (updatedSession != null && updatedSession.AccessTokenExpiresAt > DateTime.UtcNow.AddMinutes(1))
+                    {
+                        session = updatedSession;
+                    }
+                    else
+                    {
+                        await cache.RemoveAsync(sessionId);
+                        context.RejectPrincipal();
+                        return;
+                    }
+                }
+            }
+
+            context.HttpContext.Items["AccessToken"] = session.AccessToken;
+            context.ShouldRenew = true;
+        };
+    });
 
 var app = builder.Build();
 
@@ -10,5 +137,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseCors("AllowLocalFrontend");
+app.UseAuthentication(); 
+app.UseAuthorization();  
+app.MapEndpoints(Assembly.GetExecutingAssembly());
 app.Run();
