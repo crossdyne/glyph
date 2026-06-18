5a2c2f8037e3628f981dadfcae1b8a9688721cb0
Light Play <lightplay456@gmail.com>
2026-06-18 16:39:10 +0300

    feat: реализованы переиспользуемые компоненты кнопок (delete и edit)

---
+++ web/glyph-ui/src/shared/ui/edit-button/edit-button.ts
@@ -0,0 +1,22 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'edit-button',
+  imports: [],
+  templateUrl: './edit-button.html',
+  styleUrl: './edit-button.scss',
+})
+export class EditButton {
+  disabled = input(false);
+  size = input<'sm' | 'md' | 'lg'>('md');
+  type = input<'button' | 'submit' | 'reset'>('button');
+  ariaLabel = input('Удалить');
+
+  clicked = output<void>();
+
+  onClick(): void{
+    if (!this.disabled()){
+      this.clicked.emit();
+    }
+  }
+}
